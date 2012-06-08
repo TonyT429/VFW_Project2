485674b0eb0a1f879b3e878c8ab2ba52b4ff3eeb
Anthony <TonyT429@gmail.com>
2012-06-07 21:20:55 -0700

    saving what I sent in for class

--- main.js
+++ main.js
@@ -17,21 +17,21 @@ alert (localStorage.value(0));
     
     // Create the element for the select field and fill with options
     
-    function buildList ( ) {
+     function makeCats( ) {
         var genreTypes = [ "-- Choose a Genre or Subject --", "Art & Photography", "Biographies & Memoirs", "Children's Books", "Computers & Technolory", "Cookbooks, Food & Wine", "Crafts, Hobbies, and Home", "Education & Reference", "Health, Fitness & Dieting", "History", "Horror", "Humor", "Law","Literature & Fiction", "Manga & Graphic Novels", "Math & Science", "Medical", "Mystery, Crime, Thriller & Suspense", "Parenting & Relationships", "Religion & Spirituality", "Romance", "Sci Fi & Fantasy", "Self Help", "Sports & Outdoors", "Teens", "Travel", "Western"];
-        var formTag = document.getElementsByTagName( "form"),
-            selectLi = $( 'select' ),
-            makeSelect = document.createElement ( 'select' );
-            makeSelect.setAttribute( "id", "genres" );
-        for (var i=0, g=genreTypes.length; i<g; i++) { 
-            var makeOption = document.createElement( 'option' );
-            var optText = genreTypes[ i ];
-            makeOption.setAttribute( "value", optText );
-            makeOption.innerHTML = optText;
-            makeSelect.appendChild(makeOption);
-        }
-        selectLi.appendChild(makeSelect);
-    }
+        var formTag = document.getElementsByTagName( "form" ),
+            selectLi = $( 'select' ),
+            makeSelect = document.createElement( 'select' );
+            makeSelect.setAttribute( 'id', 'genres' );
+        for (var i=0, j=genreTypes.length; i<j; i++) {
+            var makeOption = document.createElement( 'option' );
+            var optText = genreTypes[ i ];
+            makeOption.setAttribute( 'value', optText );
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption );
+        }
+        selectLi.appendChild( makeSelect );
+    }
     
     
     // Get value of selected Radio Button
@@ -49,13 +49,13 @@ alert (localStorage.value(0));
     function toggleControls( n ) {
         switch( n ) {
             case "on":
-                $( 'contactForm').style.display = "none";
+                $( 'genreForm').style.display = "none";
                 $( 'clear' ).style.display = "inline";
                 $( 'displayLink' ).style.display = "none";
                 $( 'addNew').style.display = "inline";
                 break;
             case "off":
-                $( 'contactForm').style.display = "block";
+                $( 'genreForm').style.display = "block";
                 $( 'clear' ).style.display = "inline";
                 $( 'displayLink' ).style.display = "none";
                 $( 'addNew').style.display = "none";
@@ -67,8 +67,8 @@ alert (localStorage.value(0));
     }
     
     
-    function saveData ( ) {
-        var id         = Math.floor(Math.random( )*100001);           // collect and store all form field values as a object.  
+    function storeData( ) {
+        var id = Math.floor(Math.random( )*1000000001);          // collect and store all form field values as a object.  
         getSelectedRadio( );                                                         // calls the value of the radio button        
         var item     = { };                                                                      
             item.genre             = [ "Genre:", $( 'genres' ).value ];               
@@ -79,7 +79,7 @@ alert (localStorage.value(0));
             item.horror             = [ "Horror:", $( 'horror' ).value ];
             item.humor             = [ "Humor:", $( 'humor' ).value ];
             item.literature         = [ "Literature & Fiction:", $( 'literature' ).value ];
-            item.mange            = [ "Manga & Graphic Novels:", $( 'manga' ).value ];
+            item.manga            = [ "Manga & Graphic Novels:", $( 'manga' ).value ];
             item.mystery          = [ "Mystery, Crime, Thriller & Suspense:", $( 'mystery' ).value ];
             item.romance         = [ "Romance:", $( 'romance' ).value ];
             item.scifi                = [ "Sci-Fi & Fantasy:", $( 'scifi' ).value ];
@@ -112,31 +112,32 @@ alert (localStorage.value(0));
         alert ("Saved" );
     }
 
-	function showData( ) {                                         
-		toggleControls( "on" );
+	function getData ( ) {
+        toggleControls( "on" );
         if (localStorage.length === 0 ) {
             alert ( "There is no data in Local Storage." );
         }
-        // Display the data in Local Storage on the browser.
-		var makeDiv = document.createElement( 'div' );
-		makeDiv.setAttribute( "id", "items" );
-		var makeList = document.createElement( 'ul' );
-		makeDiv.appendChild(makeList);
-		document.body.appendChild(makeDiv);
-		$( 'items' ).style.display = "block";
-		for (var i=0, lstor=localStorage.length; i<lstor; i++) {
-			var makeli =document.createElement( 'li');
-			makeList.appendChild(makeli);
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			var obj = JSON.parse(value);                     // convert string data from local storage back to an object using JSON.parse()
-			var buildSubList = document.createElement ( 'ul' );
-			makeli.appendChild(buildSubList);
-			for (var q in obj) {
-				var buildSubli = document.createElement( 'li');
-				buildSubList.appendChild( buildSubli );
-				var optSubText = obj[ q ] [ 0 ]+ " "  + obj [ q ] [ 1 ];
-				buildSubli.innerHTML = optSubText;
+        // Write Data from Local Storage to the browser.
+        var makeDiv = document.createElement( 'div' );
+        makeDiv.setAttribute( "id", "items" );
+        var makeList = document.createElement( 'ul' );
+        makeDiv.appendChild( makeList );
+        document.body.appendChild( makeDiv );
+        $( 'items' ).style.display = "block";
+        for (var i=0, len=localStorage.length; i<len; i++ ) {
+            var makeli = document.createElement( 'li' );
+            makeList.appendChild( makeli );
+            var key = localStorage.key( i );
+            var value = localStorage.getItem( key );
+            // Convert the string from local storage value back to an object by using stringify
+            var obj = JSON.parse( value );
+            var makeSubList = document.createElement( 'ul' );
+            makeli.appendChild( makeSubList );
+            for (var n in obj ) {
+                var makeSubli = document.createElement( 'li' );
+                makeSubList.appendChild( makeSubli );
+                var optSubText = obj[ n ] [ 0 ]+ " " +obj[ n ] [ 1 ];
+                makeSubli.innerHTML = optSubText;
 			}
 		}
 	}
@@ -153,21 +154,21 @@ alert (localStorage.value(0));
     }
     
     // Variable defaults
-    buildList( );
+
+    var isaseries;
+    makeCats( );
 
 
     // Set Link and Submit Click Events
     
     var displayLink = $ ( 'show' );
-   displayLink.addEventListener ( 'click', showData );
+   displayLink.addEventListener ( 'click', getData );
     
     var clearLink = $ ( 'clear' );
     clearLink.addEventListener ( 'click', clearLocData );
     
     var save = $ ( 'submit' );
-    save.addEventListener ( 'click', saveData );
-
-
+    save.addEventListener ( 'click', storeData );
 
 } );
 
